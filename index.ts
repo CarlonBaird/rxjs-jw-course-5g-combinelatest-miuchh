2593986f60a6aeb482cdb3af1cdfb096834a24c8
CarlonBaird <carlyestrella@live.com>
2022-11-22 09:45:41 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
+import { fromEvent } from 'rxjs';
+
 const temperatureInput = document.getElementById('temperature-input');
 const conversionDropdown = document.getElementById('conversion-dropdown');
 const resultText = document.getElementById('result-text');
 
+const temperatureInputEvent$ = fromEvent(temperatureInput, 'input');
+const conversionInputEvent$ = fromEvent(conversionDropdown, 'input');
